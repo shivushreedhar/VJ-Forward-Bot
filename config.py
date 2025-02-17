abed4a6447b7b681e83c1bee5c5034ee809fc58b
shivushreedhar <131353533+shivushreedhar@users.noreply.github.com>
2025-02-17 21:36:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    API_ID = int(environ.get("API_ID", "16073849"))
+    API_HASH = environ.get("API_HASH", "e84dd69cd0504b8b45b2fd6a4e19068d")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7271533899:AAGsV8M7FjlvC0ZxOL3swtYfY10-JbfUW4o") 
+    BOT_SESSION = environ.get("BOT_SESSION", "Bsbot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://shivaprasadshegde06:FsvKlFtp9BcnOdoO@cluster0.6tsqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Bs-forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "5536032493"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
